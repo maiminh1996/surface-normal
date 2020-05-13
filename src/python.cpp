e00955e7b1f0b25da405cc9d327b05806cd29753
mai minh <nguyen.mai@insa-cvl.fr>
2020-05-13 21:47:28 +0200

    fix bug

--- src/python.cpp
+++ src/python.cpp
@@ -26,13 +26,9 @@ py::array_t<uint8_t> normals_from_depth_numpy_wrapper(const py::array &depth,
 PYBIND11_MODULE(surface_normal, m) {
   m.doc() = "";
 
-  m.def("normals_from_depth", &normals_from_depth_imgfile_wrapper, py::arg("depth_in_path"),
-        py::arg("normals_out_path"), py::arg("intrinsics"), py::arg("window_size") = 15,
-        py::arg("max_rel_depth_diff") = 0.1, py::arg("use_cuda") = USE_CUDA);
+  m.def("normals_from_depth", &normals_from_depth_imgfile_wrapper, py::arg("depth_in_path"), py::arg("normals_out_path"), py::arg("intrinsics"), py::arg("window_size") = 15, py::arg("max_rel_depth_diff") = 0.1, py::arg("use_cuda") = USE_CUDA);
 
-  m.def("normals_from_depth_numpy", &normals_from_depth_numpy_wrapper, py::arg("depth"),
-        py::arg("intrinsics"), py::arg("window_size") = 15, py::arg("max_rel_depth_diff") = 0.1,
-        py::arg("use_cuda") = USE_CUDA);
+  m.def("normals_from_depth_numpy", &normals_from_depth_numpy_wrapper, py::arg("depth"), py::arg("intrinsics"), py::arg("window_size") = 15, py::arg("max_rel_depth_diff") = 0.1, py::arg("use_cuda") = USE_CUDA);
 
 #ifdef VERSION_INFO
   m.attr("__version__") = VERSION_INFO;
