97829486eb602aaaab421463a801f07153dcccbe
mai minh <nguyen.mai@insa-cvl.fr>
2020-05-13 22:21:34 +0200

    fig bug ambiguous namspace

--- src/python.cpp
+++ src/python.cpp
@@ -5,7 +5,7 @@
 #include "surface_normal/numpy.hpp"
 #include "surface_normal/opencv2.hpp"
 
-namespace py = pybind11;
+//namespace py = pybind11;
 using namespace surface_normal;
 
 void normals_from_depth_imgfile_wrapper(const std::string &depth_in_path,
@@ -24,6 +24,7 @@ py::array_t<uint8_t> normals_from_depth_numpy_wrapper(const py::array &depth,
 }
 
 PYBIND11_MODULE(surface_normal, m) {
+
   m.doc() = "";
 
   m.def("normals_from_depth", &normals_from_depth_imgfile_wrapper, py::arg("depth_in_path"), py::arg("normals_out_path"), py::arg("intrinsics"), py::arg("window_size") = 15, py::arg("max_rel_depth_diff") = 0.1, py::arg("use_cuda") = USE_CUDA);
